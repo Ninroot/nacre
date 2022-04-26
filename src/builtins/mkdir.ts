41468019932d73a6ab2008b6d36db7c2bf93c976
Arnaud Debec <debec.arnaud@gmail.com>
2022-04-26 10:31:40 +0200

    Add jsdoc to mkdir.

--- src/builtins/mkdir.ts
+++ src/builtins/mkdir.ts
@@ -2,17 +2,33 @@
 
 import path = require('path');
 import { mkdirSync } from 'fs';
+import { dirPathCompleter } from '../lib/pathCompleter';
 
-const mkdir = (dirpath) => {
-  path.normalize(dirpath);
-  mkdirSync(dirpath, { recursive: false });
-  return path.normalize(dirpath);
+/**
+ * Create a directory. Wrapper of fs.mkdirSync.
+ * @param dirPath - the path of the directory to be created.
+ * @return - the path of the newly created directory.
+ */
+const mkdir = (dirPath: string): string => {
+  path.normalize(dirPath);
+  mkdirSync(dirPath, { recursive: false });
+  return path.normalize(dirPath);
 };
 
-mkdir.recursive = (dirpath) => {
-  path.normalize(dirpath);
-  mkdirSync(dirpath, { recursive: true });
-  return path.normalize(dirpath);
+mkdir.complete = [dirPathCompleter];
+
+/**
+ * Create a directory and its intermediate directories as required. Wrapper of fs.mkdirSync.
+ * @param dirPath - the path of the directory to be created.
+ * @return - the path of the newly created directory.
+ */
+mkdir.intermediate = (dirPath: string): string => {
+  path.normalize(dirPath);
+  mkdirSync(dirPath, { recursive: true });
+  return path.normalize(dirPath);
 };
+
+// @ts-ignore
+mkdir.intermediate.complete = [dirPathCompleter];
 
 export = mkdir;
