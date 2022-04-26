bc38e1e52a991f19541a3040fb3afc187bd03946
Arnaud Debec <debec.arnaud@gmail.com>
2022-04-26 11:20:35 +0200

    Add jsdoc to touch.

--- src/builtins/touch.ts
+++ src/builtins/touch.ts
@@ -1,17 +1,22 @@
 'use strict';
 
+import { closeSync, openSync, utimesSync } from 'fs';
 import path = require('path');
-import {closeSync, openSync, utimesSync } from 'fs';
 
-const touch = (filepath) => {
+/**
+ * Change item access and modification times. If any file does not exist, it is created.
+ * @param itemPath - path of the item.
+ * @return path of the item.
+ */
+const touch = (itemPath: string): string => {
   const time = new Date();
   try {
     // change file access and modification times
-    utimesSync(filepath, time, time);
+    utimesSync(itemPath, time, time);
   } catch {
-    closeSync(openSync(filepath, 'w'));
+    closeSync(openSync(itemPath, 'w'));
   }
-  return path.normalize(filepath);
-}
+  return path.normalize(itemPath);
+};
 
 export = touch;
