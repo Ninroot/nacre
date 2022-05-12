7699066df18f4925efc90ce6a583a6edc0b5416b
Arnaud Debec <debec.arnaud@gmail.com>
2022-05-12 14:56:40 +0200

    Complete npm install with version

--- src/commands/npmCompleter.ts
+++ src/commands/npmCompleter.ts
@@ -1,10 +1,27 @@
 import { spawnSync } from 'child_process';
 
-export function completeNpmPackageName(line: string) {
-  const { stdout } = spawnSync('npm', ['search', '--json', line]);
-  const packages = JSON.parse(stdout.toString());
-  const hits = packages
+function searchPackageByName(pkgName: string) {
+  const { stdout } = spawnSync('npm', ['search', '--json', pkgName]);
+  return JSON.parse(stdout.toString());
+}
+
+function searchVersionsByName(pkgName: string): string[] {
+  const { stdout: versionBuf } = spawnSync('npm', ['view', '--json', pkgName, 'versions']);
+  return JSON.parse(versionBuf.toString());
+}
+
+export function completeNpmPackageName(line: string): [string[], string] {
+  const [pkgName, pkgVersion] = line.split('@');
+  if (pkgVersion || pkgVersion === '') {
+    try {
+      const versions = searchVersionsByName(pkgName).filter((v) => v.startsWith(pkgVersion));
+      return [versions.map((v) => pkgName + '@' + v), line];
+    } catch { }
+  }
+
+  let hits = searchPackageByName(pkgName)
     .filter((p) => p.name.startsWith(line))
     .map((p) => p.name);
-  return [hits, line];
+
+  return [hits, pkgName];
 }
--- test/integration/commands/npmCompleter.test.ts
+++ test/integration/commands/npmCompleter.test.ts
@@ -7,15 +7,21 @@ import { completeNpmPackageName } from '../../../built/commands/npmCompleter';
 
 
 describe('npm unit test', () => {
-  it('completeNpmPackageName', () => {
-    // @ts-ignore
+
+  it('completeNpmPackageName with existing package', () => {
     const [hits, line] = completeNpmPackageName('chalk');
     assert.include(hits, 'chalk');
     assert.deepEqual(line, 'chalk');
   });
 
+  it('completeNpmPackageName with existing package and existing version', () => {
+    const [hits, line] = completeNpmPackageName('chalk@5');
+    console.log(hits, line);
+    hits.forEach((h) => assert.match(h, /chalk@[0-9]\.[0-9]\.[0-9]/));
+    assert.deepEqual(line, 'chalk@5');
+  });
+
   it('completeNpmPackageName non existing package', () => {
-    // @ts-ignore
     const [hits, line] = completeNpmPackageName('doesNotExist');
     assert.deepEqual(hits, []);
     assert.deepEqual(line, 'doesNotExist');
