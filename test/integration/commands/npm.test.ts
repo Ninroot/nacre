cb04ed87a04b2d1f778157475438cf41cf0fd4e2
Arnaud Debec <debec.arnaud@gmail.com>
2022-05-13 11:31:16 +0200

    Fix imports in tests.

--- test/integration/commands/npm.test.ts
+++ test/integration/commands/npm.test.ts
@@ -1,37 +1,33 @@
-'use strict';
+"use strict";
 
-import { afterEach, beforeEach, describe, it } from 'mocha';
+import { afterEach, beforeEach, describe, it } from "mocha";
 
-import { assert } from 'chai';
-import cat = require('../../../src/builtins/cat');
-import npm = require('../../../src/commands/npm');
-import path = require('path');
-import { mkdtemp } from 'fs/promises';
-import { tmpdir } from 'os';
-import { chdir } from 'process';
+import { assert } from "chai";
+import cat = require("../../../src/builtins/cat");
+import npm = require("../../../src/commands/npm");
+import path = require("path");
+import { mkdtemp } from "fs/promises";
+import { tmpdir } from "os";
+import { chdir } from "process";
 
-
-describe('npm integration test', () => {
-  beforeEach('prepare fixtures directory', async () => {
-    const tmp = await mkdtemp(path.join(tmpdir(), 'npm-'));
+describe("npm integration test", () => {
+  beforeEach("prepare fixtures directory", async () => {
+    const tmp = await mkdtemp(path.join(tmpdir(), "npm-"));
     chdir(tmp);
-    cat.overwrite('package.json', '{}');
+    cat.overwrite("package.json", "{}");
   });
 
-  afterEach('', () => {
+  afterEach("", () => {
     chdir(__dirname);
   });
 
-  it('should throw install a package that does not exist', function () {
-    assert.throws(
-      () => npm.install('doesNotExist'),
-      /Not Found - GET/,
-    );
+  it("should throw install a package that does not exist", function () {
+    assert.throws(() => npm.install("doesNotExist"), /Not Found - GET/);
   });
 
-  it('should uninstall a package that does not exit', function () {
-    const chalk = npm.uninstall('chalk');
-    assert.deepEqual(chalk.name, 'chalk');
+  it("should uninstall a package that does not exit", function () {
+    const chalk = npm.uninstall("chalk");
+    assert.deepEqual(chalk.name, "chalk");
     assert.isUndefined(chalk.version);
     assert.isUndefined(chalk.resolved);
     assert.equal(chalk.added, 0);
@@ -39,10 +35,11 @@ describe('npm integration test', () => {
     assert.equal(chalk.changed, 0);
   });
 
-  it('should install chalk', function () {
-    const chalk = 'chalk@5.0.0';
-    const { name, version, resolved, added, removed, changed } = npm.install(chalk);
-    assert.deepEqual(name, 'chalk');
+  it("should install chalk", function () {
+    const chalk = "chalk@5.0.0";
+    const { name, version, resolved, added, removed, changed } =
+      npm.install(chalk);
+    assert.deepEqual(name, "chalk");
     assert.match(version, /[0-9]+\.[0-9]+\.[0-9]+/);
     assert.isString(resolved);
     assert.equal(added, 1);
@@ -52,11 +49,12 @@ describe('npm integration test', () => {
     assert.equal(npm.install(chalk).added, 0);
   });
 
-  it('should uninstall chalk', function () {
-    assert.equal(npm.install('chalk@5.0.0').added, 1);
+  it("should uninstall chalk", function () {
+    assert.equal(npm.install("chalk@5.0.0").added, 1);
 
-    const { name, version, resolved, added, removed, changed } = npm.uninstall('chalk');
-    assert.deepEqual(name, 'chalk');
+    const { name, version, resolved, added, removed, changed } =
+      npm.uninstall("chalk");
+    assert.deepEqual(name, "chalk");
     assert.match(version, /[0-9]+\.[0-9]+\.[0-9]+/);
     assert.isString(resolved);
     assert.equal(added, 0);
--- test/integration/commands/npmCompleter.test.ts
+++ test/integration/commands/npmCompleter.test.ts
@@ -1,33 +1,34 @@
-'use strict';
+"use strict";
 
-import { describe, it } from 'mocha';
+import { describe, it } from "mocha";
 
-import { assert } from 'chai';
-import { completeNpmPackageName, completeNpmUninstallPackageName } from '../../../src/commands/npmCompleter';
+import { assert } from "chai";
+import {
+  completeNpmPackageName,
+  completeNpmUninstallPackageName,
+} from "../../../src/commands/npmCompleter";
 
-
-describe('npm integration test', () => {
-
-  it('completeNpmPackageName with existing package', () => {
-    const [hits, line] = completeNpmPackageName('chalk');
-    assert.include(hits, 'chalk');
-    assert.deepEqual(line, 'chalk');
+describe("npm integration test", () => {
+  it("completeNpmPackageName with existing package", () => {
+    const [hits, line] = completeNpmPackageName("chalk");
+    assert.include(hits, "chalk");
+    assert.deepEqual(line, "chalk");
   });
 
-  it('completeNpmPackageName with existing package and existing version', () => {
-    const [hits, line] = completeNpmPackageName('chalk@5');
+  it("completeNpmPackageName with existing package and existing version", () => {
+    const [hits, line] = completeNpmPackageName("chalk@5");
     hits.forEach((h) => assert.match(h, /chalk@[0-9]\.[0-9]\.[0-9]/));
-    assert.deepEqual(line, 'chalk@5');
+    assert.deepEqual(line, "chalk@5");
   });
 
-  it('completeNpmPackageName non existing package', () => {
-    const [hits, line] = completeNpmPackageName('doesNotExist');
+  it("completeNpmPackageName non existing package", () => {
+    const [hits, line] = completeNpmPackageName("doesNotExist");
     assert.deepEqual(hits, []);
-    assert.deepEqual(line, 'doesNotExist');
+    assert.deepEqual(line, "doesNotExist");
   });
 
-  it('completeNpmUninstallPackageName', () => {
-    const [hits] = completeNpmUninstallPackageName('');
-    ['mocha', 'chai'].forEach((expected) => assert.include(hits, expected));
+  it("completeNpmUninstallPackageName", () => {
+    const [hits] = completeNpmUninstallPackageName("");
+    ["mocha", "chai"].forEach((expected) => assert.include(hits, expected));
   });
 });
