751ab83a73f10dbd3e9fa1bd7878e3bcd6bb787a
Arnaud Debec <debec.arnaud@gmail.com>
2022-06-20 16:31:30 +0200

    Upgrade emphasize to 4.2.0 to remove vulnerabilities.

--- src/highlight.js
+++ src/highlight.js
@@ -1,44 +1,43 @@
-'use strict';
+"use strict";
 
-const emphasize = require('emphasize');
-const chalk = require('chalk');
+const emphasize = require("emphasize");
+const chalk = require("chalk");
 
-const windows = process.platform === 'win32';
+const windows = process.platform === "win32";
 const sheet = {
-  'comment': chalk.gray,
-  'quote': chalk.gray,
+  comment: chalk.gray,
+  quote: chalk.gray,
 
-  'keyword': chalk.green,
-  'addition': chalk.green,
+  keyword: chalk.green,
+  addition: chalk.green,
 
-  'number': windows ? chalk.yellow : chalk.blue,
-  'string': chalk.green,
-  'meta meta-string': chalk.cyan,
-  'literal': chalk.cyan,
-  'doctag': chalk.cyan,
-  'regexp': chalk.cyan,
+  number: windows ? chalk.yellow : chalk.blue,
+  string: chalk.green,
+  "meta meta-string": chalk.cyan,
+  literal: chalk.cyan,
+  doctag: chalk.cyan,
+  regexp: chalk.cyan,
 
-  'attribute': undefined,
-  'attr': undefined,
-  'variable': chalk.yellow,
-  'template-variable': chalk.yellow,
-  'class title': chalk.yellow,
-  'type': chalk.yellow,
+  attribute: undefined,
+  attr: undefined,
+  variable: chalk.yellow,
+  "template-variable": chalk.yellow,
+  "class title": chalk.yellow,
+  type: chalk.yellow,
 
-  'symbol': chalk.magenta,
-  'bullet': chalk.magenta,
-  'subst': chalk.magenta,
-  'meta': chalk.magenta,
-  'meta keyword': chalk.magenta,
-  'link': chalk.magenta,
+  symbol: chalk.magenta,
+  bullet: chalk.magenta,
+  subst: chalk.magenta,
+  meta: chalk.magenta,
+  "meta keyword": chalk.magenta,
+  link: chalk.magenta,
 
-  'built_in': chalk.cyan,
-  'deletion': chalk.red,
+  built_in: chalk.cyan,
+  deletion: chalk.red,
 
-  'emphasis': chalk.italic,
-  'strong': chalk.bold,
-  'formula': chalk.inverse,
+  emphasis: chalk.italic,
+  strong: chalk.bold,
+  formula: chalk.inverse,
 };
 
-module.exports = (s) =>
-  emphasize.highlight('js', s, sheet).value;
+module.exports = (s) => emphasize.highlight("js", s, sheet).value;
